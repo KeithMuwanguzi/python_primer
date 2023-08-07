892d381cca52c7a4f564bc7d964f8fcfd2afb9f6
KeithAnnonMM <keithmuwanguzi@gmail.com>
2023-08-07 22:39:44 +0300

    Flow control

--- basics.py
+++ basics.py
@@ -22,3 +22,12 @@ z = x
 
 value = 0b011
 print(value,'is of the type:',type(value))
+
+print('Good Morning', end=' ')
+print('I hope you are kawa')
+print('New Year',2020,'Is here',sep='-')
+x = 1
+y = "the value is:"
+z =input('Enter the value:')
+
+print('For the developed code, {}{}. For all the others, the value is: {}'.format(y,x,z))
---
+++ conditionals.py
@@ -0,0 +1,8 @@
+print('Good Morning', end=' ')
+print('I hope you are kawa')
+print('New Year',2020,'Is here',sep='-')
+x = 1
+y = "the value is:"
+z =input('Enter the value:')
+
+print('For the developed code, {}{}. For all the others, the value is: {}'.format(y,x,z))
