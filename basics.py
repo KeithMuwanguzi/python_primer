12706375b5dc4256ceaf8f67970c0ca3d6128e91
KeithAnnonMM <keithmuwanguzi@gmail.com>
2023-08-07 22:15:59 +0300

    Varibles and datatypes

--- basics.py
+++ basics.py
@@ -1,1 +1,24 @@
-print('Hello World')
+import constants
+
+#Basic Syntax
+print('Let\'s get this started')
+
+#Variables and data types
+num = 1
+name = 'Keith Muwanguzi'
+# print(name)
+
+a,b,c = 1,2,"Home"
+some_binary = 0b001
+result = True
+# print(c)
+
+x = y = 'Data Unloaded'
+z = x
+# print(x,y,z)
+
+# print(constants.PI)
+# print(some_binary)
+
+value = 0b011
+print(value,'is of the type:',type(value))
