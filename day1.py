1d3990215156fe0ec9c02685e58da1b6ce1b250b
MarthaMusiimire <musiimiremartha@gmail.com>
2023-08-08 15:59:17 +0300

    Python basics

--- day1.py
+++ day1.py
@@ -35,10 +35,6 @@ elif temp > 37:
 else:
     print("very normal")
 
-i = 1
-while i<=10:
-    print(i* '@')
-    i = i+1
 
 #Lists
 food = ["rice","meat","gnuts","beef"]
@@ -49,4 +45,41 @@ food[0] = "chicken"
 print(food[0:3])
 #list methods
 digits = [1,2,3,4,5,6]
-digits.insert(7)
+digits.insert(6,7)
+digits.remove(2)
+#digits.clear()
+print(1 in digits)
+print(digits)
+print (len(digits))
+
+#loops
+ #while loops
+i = 1
+while i<=10:
+    print(i* '@')
+    i = i+1
+numbers = [1,2,3,4,5]
+
+#for loops
+for item in digits:
+    print(item)
+
+#range()function
+numbers = range(5) #print range of numbers minus five
+print(numbers)
+for number in numbers:
+    print(number)
+
+digit = range(5,10)#prints numbers from 5 to 10 except 10
+for num in digit:
+    print (num)
+
+numb = range(0,11,2) #2 is the step number, we skip two steps then go to the next number.
+for numbe in numb:
+    print(numbe)
+
+#tuples
+#these are like lists and are used to store information but they are immutable
+
+num1 = (1,2,3,4,2,2,)
+print(num1.count(2))
